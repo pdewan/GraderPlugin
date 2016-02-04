7c6fbcfc592374da087f1937379a3bd9df141a19
avitkus <avitkus7@gmail.com>
2016-02-04 14:14:02 -0500

    Updated to new server location

--- GraderProgramPlugin/src/com/unc/cs/graderprogramplugin/com/GradingServerConstants.java
+++ GraderProgramPlugin/src/com/unc/cs/graderprogramplugin/com/GradingServerConstants.java
@@ -8,8 +8,8 @@ public class GradingServerConstants {
 	//public static final int COM_PORT = 42420;
 	public static final int COM_PORT = 26501;
 	//public static final String GRADER_SERVER = "127.0.0.1";
-	public static final String GRADER_SERVER = "https://wwwp.cs.unc.edu";
-	public static final String SUBMISSION_PATH = "~vitkus/grader/submit.php";
+	public static final String GRADER_SERVER = "https://grader.cs.unc.edu";
+	public static final String SUBMISSION_PATH = "submit.php";
 	public static final int TIMEOUT = 1000;
 	public static final int BUFFER_SIZE = 512;
 }
